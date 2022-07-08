6ea7bc5aa836dfd531d23ffa0a8d84161bb0e8b8
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-08 22:18:28 +0900

    create server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/api', (req, res) => {
+  res.json({
+    "users": [
+      "userOne",
+      "userTwo",
+      "userThree"
+    ]
+  })
+})
+
+app.listen(5001, () => {
+  console.log('server started on port 5001')
+})
